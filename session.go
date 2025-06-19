10844edb9c318618aed1ccfaf2cf8a630f22837d
Volodymyr Boiko <volodymyr.boiko@vastdata.com>
2025-06-19 10:02:38 +0300

    better error for unreachable host; FillFn callback for custom structs;

--- session.go
+++ session.go
@@ -191,6 +191,7 @@ func setupHeaders(s RESTSession, r *http.Request) error {
 func doRequest(ctx context.Context, s *VMSSession, verb, url string, body Params) (Renderable, error) {
 	// callerExist if request is processed via "request" method
 	var (
+		config            = s.GetConfig()
 		resourceCaller    InterceptableVastResourceAPI
 		requestData       io.Reader
 		beforeRequestData io.Reader
@@ -232,7 +233,7 @@ func doRequest(ctx context.Context, s *VMSSession, verb, url string, body Params
 	if responseErr != nil {
 		return nil, fmt.Errorf("failed to perform %s request to %s, error %v", verb, url, responseErr)
 	}
-	if err = validateResponse(response); err != nil {
+	if err = validateResponse(response, config.Host, config.Port); err != nil {
 		return nil, err
 	}
 	result, err := unmarshalToRecordUnion(response)
